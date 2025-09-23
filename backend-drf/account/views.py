571513f1347f619b5822d7b68bfbddcfbad4d647
Prassun <pduggar@sparkcognition.com>
2025-09-23 12:31:06 +0530

    backend changes for protection routes

--- backend-drf/account/views.py
+++ backend-drf/account/views.py
@@ -3,8 +3,20 @@ from .serializers import UserSerializers
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=UserSerializers
     permission_classes=[AllowAny]
+
+class ProtectedView(APIView):
+    permission_classes=[IsAuthenticated]
+
+    def get(self,request):
+        user = request.user
+        responce = {
+            'status':f'Request was permitted {user.username}'
+        }
+        return Response(responce)
